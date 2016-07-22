5bb4d9b7fdaf4387233db205ab0e984f728bc86f
weiweilong <weiweilong_email@foxmail.com>
2016-07-22 15:33:15 +0800

    Initial Commit

---
+++ Camera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Camera
+//
+//  Created by 魏唯隆 on 16/7/22.
+//  Copyright © 2016年 魏唯隆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Camera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Camera
+//
+//  Created by 魏唯隆 on 16/7/22.
+//  Copyright © 2016年 魏唯隆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
